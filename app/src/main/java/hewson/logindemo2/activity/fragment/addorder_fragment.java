d5523e43a735583fd6893f06f0705bbf8fe9f47f
chenhewson <hewson.chen@foxmail.com>
2020-03-20 19:53:44 +0800

    添加任务信息界面完成。
    Signed-off-by: chenhewson <hewson.chen@foxmail.com>

--- app/src/main/java/hewson/logindemo2/activity/fragment/addorder_fragment.java
+++ app/src/main/java/hewson/logindemo2/activity/fragment/addorder_fragment.java
@@ -4,17 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.baidu.location.LocationClient;
+
 import hewson.logindemo2.R;
 
 public class addorder_fragment extends Fragment {
+
+    public LocationClient mLocationClient;
+    //private MapView mapview;
+    private TextView positionText;
+    private StringBuilder currentPosition;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_addorder,container,false);
     }
+
+    public addorder_fragment(){}
 }
