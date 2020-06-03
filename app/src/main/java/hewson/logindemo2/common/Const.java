2d7f94dd0b5635304ae31e18ab02bc244a2823ad
chenhewson <hewson.chen@foxmail.com>
2020-06-03 08:39:23 +0800

    毕设答辩结束，爷的青春也结束了。答辩时间：2020/05/30
    Signed-off-by: chenhewson <hewson.chen@foxmail.com>

--- app/src/main/java/hewson/logindemo2/common/Const.java
+++ app/src/main/java/hewson/logindemo2/common/Const.java
@@ -2,10 +2,10 @@ package hewson.logindemo2.common;
 
 public class Const {
     //本地ip
-    public static final String IpAddress="http://192.168.68.236:8080/";
+//    public static final String IpAddress="http://192.168.68.236:8081/";
 
 //    //阿里云
-//    public static final String IpAddress="http://101.132.143.174:8080/";
+    public static final String IpAddress="http://101.132.143.174:8081/";
 
     public static double LONGITUDE = 118.085730;//经度,写死，厦门理工，如果定位功能没问题，会自动修改这个值。
     public static double LATITUDE = 24.623940;//纬度
